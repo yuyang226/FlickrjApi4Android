e4d928088e992b78bd2c50a94dac41c19e4ede78
Toby Yu <yuyang226@gmail.com>
2011-10-24 15:00:31 +0800

    adding lock for image cache

--- flickrj-android-sample-android/src/com/gmail/yuyang226/flickrj/sample/android/images/ImageCache.java
+++ flickrj-android-sample-android/src/com/gmail/yuyang226/flickrj/sample/android/images/ImageCache.java
@@ -1,15 +1,34 @@
 package com.gmail.yuyang226.flickrj.sample.android.images;
 
 import java.lang.ref.SoftReference;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import android.graphics.Bitmap;
 
 public final class ImageCache {
 
-	private static final HashMap<String, SoftReference<Bitmap>> cache = new HashMap<String, SoftReference<Bitmap>>();
-	
-    /**
+	private static final Map<String, SoftReference<Bitmap>> cache = new LinkedHashMap<String, SoftReference<Bitmap>>() {
+		private static final long serialVersionUID = 1L;
+
+		/* (non-Javadoc)
+		 * @see java.util.LinkedHashMap#removeEldestEntry(java.util.Map.Entry)
+		 */
+		@Override
+		protected boolean removeEldestEntry(
+				Entry<String, SoftReference<Bitmap>> eldest) {
+			return size() > 50;
+		}
+
+	};
+
+	private static final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+	private static final Lock read  = readWriteLock.readLock();
+	private static final Lock write = readWriteLock.writeLock();
+
+	/**
 	 * 
 	 */
 	private ImageCache() {
@@ -17,18 +36,33 @@ public final class ImageCache {
 	}
 
 	public static Bitmap getFromCache(String id){
-        if(!cache.containsKey(id))
-            return null;
-        SoftReference<Bitmap> ref=cache.get(id);
-        return ref.get();
-    }
-    
-    public static void saveToCache(String id, Bitmap bitmap){
-        cache.put(id, new SoftReference<Bitmap>(bitmap));
-    }
+		read.lock();
+		try {
+			if(!cache.containsKey(id))
+				return null;
+			SoftReference<Bitmap> ref=cache.get(id);
+			return ref.get();
+		} finally {
+			read.unlock();
+		}
+	}
 
-    public static void clear() {
-        cache.clear();
-    }
+	public static void saveToCache(String id, Bitmap bitmap){
+		write.lock();
+		try {
+			cache.put(id, new SoftReference<Bitmap>(bitmap));
+		} finally {
+			write.unlock();
+		}
+	}
+
+	public static void clear() {
+		write.lock();
+		try {
+			cache.clear();
+		} finally {
+			write.unlock();
+		}
+	}
 
 }
