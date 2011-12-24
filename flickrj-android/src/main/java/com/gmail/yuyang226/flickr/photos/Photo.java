b59b86d2c06b61268bad00fb086c02e355382232
Toby Yu <yuyang226@gmail.com>
2011-12-24 19:22:40 +0800

    fixed the Photo equals issue

--- flickrj-android/src/main/java/com/gmail/yuyang226/flickr/photos/Photo.java
+++ flickrj-android/src/main/java/com/gmail/yuyang226/flickr/photos/Photo.java
@@ -782,13 +782,7 @@ public class Photo {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result
-				+ ((datePosted == null) ? 0 : datePosted.hashCode());
-		result = prime * result
-				+ ((dateTaken == null) ? 0 : dateTaken.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + (publicFlag ? 1231 : 1237);
-		result = prime * result + ((tags == null) ? 0 : tags.hashCode());
 		result = prime * result + ((title == null) ? 0 : title.hashCode());
 		result = prime * result + ((url == null) ? 0 : url.hashCode());
 		return result;
@@ -806,27 +800,10 @@ public class Photo {
 		if (!(obj instanceof Photo))
 			return false;
 		Photo other = (Photo) obj;
-		if (datePosted == null) {
-			if (other.datePosted != null)
-				return false;
-		} else if (!datePosted.equals(other.datePosted))
-			return false;
-		if (dateTaken == null) {
-			if (other.dateTaken != null)
-				return false;
-		} else if (!dateTaken.equals(other.dateTaken))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
-			return false;
-		if (publicFlag != other.publicFlag)
-			return false;
-		if (tags == null) {
-			if (other.tags != null)
-				return false;
-		} else if (!tags.equals(other.tags))
 			return false;
 		if (title == null) {
 			if (other.title != null)
@@ -841,5 +818,6 @@ public class Photo {
 		return true;
 	}
 
+	
 
 }
